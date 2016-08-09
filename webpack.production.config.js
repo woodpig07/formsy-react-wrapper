d157ca323cd9b762cebcc875970db51ad58e0b26
KateWu <woodpig07@gmail.com>
2016-08-09 15:17:37 +0800

    add build script

---
+++ webpack.production.config.js
@@ -0,0 +1,24 @@
+var path = require('path')
+var webpack = require('webpack')
+const ProgressBar = require('progress-bar-webpack-plugin')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, 'src/FormsyWrapper.js'),
+  output: {
+    filename: 'formsy-react-wrapper.js',
+    path: path.resolve(__dirname, 'build'),
+    library: 'FormsyWrapper',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new ProgressBar()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  }
+}
